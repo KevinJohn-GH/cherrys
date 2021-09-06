3883a58331d0dce6cd9207a518569f01be325395
Xiansong Wu （吴先松） <xiansongwu@trip.com>
2021-09-06 20:15:45 +0800

    multi redis instances

--- cherrys.py
+++ cherrys.py
@@ -7,22 +7,41 @@ Relies on redis-py https://github.com/andymccurdy/redis-py.
 """
 
 import threading
+import logging
+import sys
 
 try:
-  import cPickle as pickle
+    import cPickle as pickle
 except ImportError:
-  import pickle
+    import pickle
 
 from cherrypy.lib.sessions import Session
+from concurrent.futures import ThreadPoolExecutor, wait
 import redis
 
-class RedisSession(Session):
+log = logging.getLogger(__name__)
 
-    # the default settings
-    host = '127.0.0.1'
-    port = 6379
-    db = 0
-    password = None
+
+def _wait_first_succeed(futures, fn):
+    ret = None
+    while len(futures) > 0:
+        done, not_done = wait(futures, return_when='FIRST_COMPLETED')
+        for done_future in done:
+            exception = done_future.exception()
+            if exception:
+                if type(exception) == AssertionError:
+                    log.debug('[{fn}] Future Exception: {exception}'.format(fn=fn, exception=exception))
+                else:
+                    log.error('[{fn}] Future Exception: {exception}'.format(fn=fn, exception=exception))
+                continue
+            else:
+                ret = done_future.result()
+                return ret
+        futures = list(not_done)
+    return ret
+
+
+class RedisSession(Session):
 
     @classmethod
     def setup(cls, **kwargs):
@@ -34,35 +53,71 @@ class RedisSession(Session):
         for k, v in kwargs.items():
             setattr(cls, k, v)
 
-        cls.cache = redis.StrictRedis(
-            host=cls.host,
-            port=cls.port,
-            db=cls.db,
-            password=cls.password)
+        redis_instances = cls.redis_instances
+        cls.caches = []
+
+        for redis_instance in redis_instances:
+            cache = redis.StrictRedis(
+                host=redis_instance['host'],
+                port=redis_instance['port'],  # cherrys in charge of converting str to int
+                db=redis_instance['db'],
+                password=redis_instance.get('password', None),
+                socket_timeout=redis_instance.get('socket_timeout', 10),
+                socket_connect_timeout=redis_instance.get('socket_connect_timeout', 1))
+            cls.caches.append(cache)
+
+    def _exists_worker(self, cache):
+        result = bool(cache.exists(self.id))
+        if result:
+            return result
+        else:
+            kwargs = cache.connection_pool.connection_kwargs
+            raise AssertionError('Key {id} not exist in redis://{host}:{port}/{db}'.format(
+                id=self.id, host=kwargs['host'], port=kwargs['port'], db=kwargs['db']))
 
     def _exists(self):
-        return bool(self.cache.exists(self.id))
+        executor = ThreadPoolExecutor(max_workers=10)
+        futures = [executor.submit(fn=self._exists_worker, cache=cache) for cache in self.caches]
+        result = _wait_first_succeed(futures=futures, fn=sys._getframe().f_code.co_name)
+        return bool(result)
+
+    def _load_worker(self, cache):
+        result = cache.get(self.id)
+        if not result:
+            raise AssertionError('Get key {id} return {result}'.format(id=self.id, result=result))
+        return pickle.loads(result)
 
     def _load(self):
-        try:
-          return pickle.loads(self.cache.get(self.id))
-        except TypeError:
-          # if id not defined pickle can't load None and raise TypeError
-          return None
+        executor = ThreadPoolExecutor(max_workers=10)
+        futures = [executor.submit(fn=self._load_worker, cache=cache) for cache in self.caches]
+        result = _wait_first_succeed(futures=futures, fn=sys._getframe().f_code.co_name)
+        return result
 
-    def _save(self, expiration_time):
+    def _save_worker(self, cache, expiration_time):
 
         pickled_data = pickle.dumps(
             (self._data, expiration_time),
             pickle.HIGHEST_PROTOCOL)
 
-        result = self.cache.setex(self.id, self.timeout * 60, pickled_data)
+        result = cache.setex(self.id, pickled_data, self.timeout * 60)
 
         if not result:
-            raise AssertionError("Session data for id %r not set." % self.id)
+            kwargs = cache.connection_pool.connection_kwargs
+            raise AssertionError('Session data for id {id} not set into redis://{host}:{port}/{db}.'.format(
+                id=self.id, host=kwargs['host'], port=kwargs['port'], db=kwargs['db']))
+
+    def _save(self, expiration_time):
+        executor = ThreadPoolExecutor(max_workers=10)
+        futures = [executor.submit(fn=self._save_worker, cache=cache, expiration_time=expiration_time)
+                   for cache in self.caches]
+        _wait_first_succeed(futures=futures, fn=sys._getframe().f_code.co_name)
+
+    def _delete_worker(self, cache):
+        cache.delete(self.id)
 
     def _delete(self):
-        self.cache.delete(self.id)
+        executor = ThreadPoolExecutor(max_workers=10)
+        futures = [executor.submit(fn=self._delete_worker, cache=cache) for cache in self.caches]
 
     # http://docs.cherrypy.org/dev/refman/lib/sessions.html?highlight=session#locking-sessions
     # session id locks as done in RamSession
@@ -78,4 +133,3 @@ class RedisSession(Session):
         """Release the lock on the currently-loaded session data."""
         self.locks[self.id].release()
         self.locked = False
-
